b14cf790295858febeed78d5cdf6b5ad68d85ac9
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2023-04-11 16:52:41 +0300

    almost

--- js/main.js
+++ js/main.js
@@ -51,11 +51,11 @@ window.addEventListener("scroll", function () {
 
 
 // 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
